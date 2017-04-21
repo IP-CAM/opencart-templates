b6e3dcbfe3cf73d3bea184e2f3aeff3e98a6eec4
LinguistMB <MariaBerkova.MB@gmail.com>
2017-04-21 14:36:43 +0300

    Add files via upload

--- sections/extensions/ru/module_twitter.php
+++ sections/extensions/ru/module_twitter.php
@@ -29,10 +29,6 @@
 <ul class="marked-list">
   <li> <strong>Module Name</strong> — название модуля;</li>
   <li><strong>Page URL</strong> —  в этом поле, задайте URL той Twitter  страницы, которую хотите добавить на ваш сайт;</li>
-  <li>
-    <div align="justify"><strong>Widget ID</strong> — Widget ID, который вы можете получить                  <a href="https://twitter.com/settings/widgets" target="_blank">здесь</a>; </div>
-  </li>
-  <li><strong>Tweet Limit</strong> —  количество твитов;</li>
   <li><strong>Color Scheme</strong> — цветовая схема (светлая, темная);</li>
   <li> <strong>Width</strong> — ширина блока в пикселях; </li>
   <li> <strong>Height</strong> — высота блока в пикселях; </li>
@@ -58,21 +54,8 @@
 <ul class="marked-list">
   <li> <strong>Module Name</strong> —  укажите название модуля;</li>
   <li><strong> Page URL</strong> —  в этом поле, задайте URL той  страницы, которую хотите добавить на ваш сайт;</li>
-  <li>
-    <div align="justify"><strong>Widget ID</strong> — укажите идентификатор приложения  Twitter в этом поле; </div>
-  </li>
-  <li><strong>Tweet Limit</strong> —  задайте нужное количество отображаемых твитов в виджете;</li>
   <li><strong>Color Scheme</strong> — выберите цветовую схему (светлую, темную);</li>
   <li> <strong>Width</strong> —задайте значение ширины блока в пикселях; </li>
   <li> <strong>Height</strong> — задайте значение высоты блока в пикселях; </li>
   <li> <strong>Status</strong> — укажите статус модуля (включен/отключен). </li>
 </ul>
-
-<h5>Как получить  Twitter widget ID</h5>
-<p align="justify">Если Twitter виджет, который  пытаетесь активировать, запрашивает   Twitter widget ID, вам необходимо будет зарегистрировать новый виджет в системе Twitter, перейдя по <a href="https://twitter.com/settings/widgets" onclick="__gaTracker['send', 'event', 'outbound-article', 'https://twitter.com/settings/widgets', 'Twitter widget settings');">этой ссылке</a>.</p>
-<p align="justify">Чтобы получить доступ к настройкам виджета Twitter, необходимо войти в учетную запись Twitter, которая будет выводиться в виджете на вашем сайте. Затем,  в правом верхнем углу, нажмите на кнопку с изображением шестеренки и выберите <strong>Settings</strong> и перейдите во вкладку <strong>Widgets</strong>, что расположена в самом низу боковой панели меню. </p>
-<p align="justify">В появившемся окне, нажмите кнопку <strong>Create new</strong>.   Вы приступите к процессу создания [регистрации]. Далее, вам будут предложено настроить виджет, используя доступные параметры конфигурации. Здесь также  доступна функция предварительного просмотра этого виджета. Помните, вы всегда сможете вернуться   к настройкам параметров виджета.</p>
-<p align="justify">Кнопка <strong>Create new</strong> будет заменена на кнопку <strong>Save changes</strong>. Теперь, когда  виджет добавлен в систему Twitter, вы можете получить его ID. В адресную строке браузера вы увидите адрес URL, который выглядит примерно так:</p>
-<blockquote><p>https://twitter.com/settings/widgets/<strong>512253083833733120</strong>/edit?focus_textarea=1&amp;notice=WIDGET_CREATED</p> </blockquote>
-<p> ID вашего виджета состоит из  18 цифр. В этом случае это — <strong>512253083833733120</strong>. Сохраняйте ID в тайне. </p>
-<p align="justify">Скопируйте ID и внесите в соответствующее поле в настройках модуля. После того, как настройки будут сохранены, виджет активируется на вашем сайте.</p>
