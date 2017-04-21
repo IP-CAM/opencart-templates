26ba65b238a6b9b11a9c803f9a152335cf914be7
LinguistMB <MariaBerkova.MB@gmail.com>
2017-04-21 14:36:09 +0300

    change twitter

--- sections/extensions/en/module_twitter.php
+++ sections/extensions/en/module_twitter.php
@@ -29,13 +29,8 @@
 
 <ul class="marked-list">
   <li> <strong>Module Name</strong> — the name of the module;</li>
-  <li><strong>Page URL</strong> — allows specifying custom Twitter Page URL;</li>
-  <li>
-    <div align="justify"><strong>Widget ID</strong> — you can get the Widget ID
-    <a href="https://twitter.com/settings/widgets" target="_blank">here</a>; </div>
-  </li>
+  <li><strong>Twitter Page URL</strong> — allows specifying custom Twitter Page URL;</li>
   <li><strong>Tweet Limit</strong> —  number of tweets;</li>
-  <li><strong>Color Scheme</strong> — color scheme (light, dark);</li>
   <li><strong>Width</strong> — the pixel width of the widget;</li>
   <li><strong>Height</strong> — the pixel height of the widget; </li>
   <li><strong>Status</strong> — define the module status (enabled/disabled).</li>
@@ -62,19 +57,8 @@
 <ul class="marked-list">
   <li><strong>Module Name</strong> — enter the name of the module;</li>
   <li><strong>Page URL</strong> — specify custom Twitter Page URL;</li>
-  <li><div align="justify"><strong>Widget ID</strong> — enter  the Widget ID; </div></li>
-  <li><strong>Tweet Limit</strong> —  set a number of tweets;</li>
   <li><strong>Color Scheme</strong> — select the color scheme (light, dark);</li>
   <li> <strong>Width</strong> — set the pixel width of the widget;</li>
   <li> <strong>Height</strong> — set the pixel height of the widget; </li>
   <li> <strong>Status</strong> — define the module status (enabled/disabled).</li>
 </ul>
-
-<h5>Find your Twitter widget ID</h5>
-<p align="justify">If a Twitter widget you are attempting to enable asks for a Twitter widget ID, you will need to create a new widget at your <a href="https://twitter.com/settings/widgets" onclick="__gaTracker('send', 'event', 'outbound-article', 'https://twitter.com/settings/widgets', 'Twitter widget settings');">Twitter widget settings</a> page.</p>
-<p align="justify">To access the Twitter widget settings, log into the Twitter account that you want to use with your widget. Then click the gear in the top right-hand corner and click <strong>Settings</strong>. From the settings' sidebar, pick the <strong>Widgets</strong> tab at the very bottom.</p>
-<p align="justify">Locate the <strong>Create new</strong> button on this page and press it to create a new widget. From here, you will be offered configuration options and a preview of the widget. You can always change the widget's configuration options later.</p>
-<p align="justify">After you've created the widget, the <strong>Create new</strong> button will be replaced by a <strong>Save changes</strong> button. Now that your widget is created, you can grab the widget ID from the URL. Look at your browser's address bar. It should have a URL that looks something like this:</p>
-<blockquote><p>https://twitter.com/settings/widgets/<strong>512253083833733120</strong>/edit?focus_textarea=1&amp;notice=WIDGET_CREATED</p> </blockquote>
-<p align="justify">Your widget ID is the 18 digit number. In this example it's <strong>512253083833733120</strong>. Remember to keep this number private, so your widget will not be used by others without your permission.</p>
-<p>Copy your ID and paste it to the text field that asks for your widget ID. After saving the settings, your new Twitter widget should be functional.</p>
