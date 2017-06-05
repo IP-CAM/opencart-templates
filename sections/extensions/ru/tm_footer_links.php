56a7105f7033bbc36bff88dc844ec8522743df21
LinguistMB <Linguist@templatemonster.me>
2017-06-05 12:02:51 +0300

    add tm_footer_links ru/en

---
+++ sections/extensions/ru/tm_footer_links.php
@@ -0,0 +1,35 @@
+<h3> TM Footer Links</h3>
+<h4 align="justify">Как управлять модулем  TM Footer Links</h4>
+<p>Перейдите к разделу <strong>Модули / Расширения</strong> (Extensions) &gt; <strong>Модули / Расширения</strong> (Extensions).</p>
+<p>
+  <figure class="img-polaroid"> <img src="img/edit-parallax-1.jpg" alt=""/></figure>
+</p>
+<p>Выберите <strong>Модули</strong> (Modules) в выпадающем списке  фильтра поиска <strong>по типу расширения</strong> (еxtension type).</p>
+<p>
+  <figure class="img-polaroid"> <img src="img/edit-parallax-2.jpg" alt=""/></figure>
+</p>
+<p>В таблице с перечнем всех модулей, найдите раздел <strong>TM Footer Link</strong>. Нажав кнопку &quot;редактировать&quot; (edit), вы перейдете к окну настроек существующего модуля.</p>
+<p align="justify" class="alert alert-info">Используйте сочетание клавиш <strong>Ctrl+F</strong> для упрощения процесса поиска.</p>
+<p>
+  <figure class="img-polaroid"> <img src="img/footer-2.jpg" alt=""/></figure>
+</p>
+<p>Вы перейдете к следующему окну настроек:</p>
+<figure class="img-polaroid">
+  <img src="img/footer-3.jpg" alt=""/>
+</figure>
+<ul class="marked-list">
+  <li><strong>Module Name</strong> — название модуля;</li>
+  <li><strong>Links Group</strong> — выберите группу ссылок, которую планируете отображать в футере;</li>
+  <li> <strong>Status</strong> — cтатус модуля (вкл./выкл.). </li>
+</ul>
+<h5>Frontend:</h5>
+<p>
+  <figure class="img-polaroid">
+    <img src="img/footer-4.jpg" alt=""/>
+  </figure>
+</p>
+<ul class="marked-list">
+  <li><strong>Module Name</strong> — TM Footer Links Information;</li>
+  <li><strong>Links Group</strong> — Information;</li>
+  <li> <strong>Status</strong> — enabled (активирован). </li>
+</ul>
