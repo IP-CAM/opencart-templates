30262ec4e388dbf2defe01a0f1f485b4b2bf2d51
LinguistMB <Linguist@templatemonster.me>
2017-06-05 11:49:09 +0300

    add tm_footer_links (en/ru)

--- sections/extensions/en/tm_footer_links.php
+++ sections/extensions/en/tm_footer_links.php
@@ -1,5 +1,5 @@
-<h3><a href="//special.olark.com/cDqdW">Olark Live Chat</a></h3>
-<h4 align="justify">How to manage Olark Live Chat module</h4>
+<h3> TM Footer Links</h3>
+<h4 align="justify">How to manage TM Footer Links module</h4>
 <p>Navigate to <strong>Extensions</strong> &gt; <strong>Extensions</strong>.</p>
 <p>
   <figure class="img-polaroid"> <img src="img/edit-parallax-1.jpg" alt=""/></figure>
@@ -8,112 +8,28 @@
 <p>
   <figure class="img-polaroid"> <img src="img/edit-parallax-2.jpg" alt=""/></figure>
 </p>
-<p>Find the <strong>Olark Live Chat </strong>section. Press the <strong>edit</strong> button to configure the existing module.</p>
+<p>Find the <strong>TM Footer Link</strong> section. Press the <strong>edit</strong> button to configure the existing module.</p>
 <p align="justify" class="alert alert-info">Use <strong>Ctrl+F</strong> to simplify the search process.</p>
 <p>
-  <figure class="img-polaroid"> <img src="img/olark-view-2.jpg" alt=""/></figure>
+  <figure class="img-polaroid"> <img src="img/footer-2.jpg" alt=""/></figure>
 </p>
 <p>You will be brought to the settings window:</p>
 <figure class="img-polaroid">
-  <img src="img/olark-view-3.jpg" alt=""/>
+  <img src="img/footer-3.jpg" alt=""/>
 </figure>
-<h5>Available settings:</h5>
 <ul class="marked-list">
   <li><strong>Module Name</strong> — the name of the module;</li>
-  <li><strong>Your Site ID</strong> — website ID that can be found on the
-  <a href='//www.olark.com/install' rel="nofollow" target="_blank">https://www.olark.com/install</a> page after you register and fill the required fields;</li>
+  <li><strong>Links Group</strong> — choose a group of links you want to display in the footer;</li>
   <li> <strong>Status</strong> — define the module status (enabled/disabled). </li>
 </ul>
-
-<h4 align="justify">How to add a new Olark Live Chat module</h4>
-<p>Navigate to <strong>Extensions</strong> &gt; <strong>Extensions</strong>.</p>
+<h5>Frontend:</h5>
 <p>
-  <figure class="img-polaroid"> <img src="img/edit-parallax-1.jpg" alt=""/></figure>
+  <figure class="img-polaroid">
+    <img src="img/footer-4.jpg" alt=""/>
+  </figure>
 </p>
-<p>Choose <strong>Modules</strong> in the <strong>Extension type</strong> dropdown.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/edit-parallax-2.jpg" alt=""/></figure>
-</p>
-<p align="justify">Find the <strong>Olark Live Chat</strong> section. Press the<strong> Add New </strong>button to create a new module.</p>
-<p align="justify" class="alert alert-info">Use <strong>Ctrl+F</strong> to simplify the search process.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/olark-view-4.jpg" alt=""/></figure>
-</p>
-<p>You will be brought to the settings window:</p>
-<figure class="img-polaroid"> <img src="img/olark-view-5.jpg" alt=""/></figure>
-<p>Fill in the required fields:</p>
-
 <ul class="marked-list">
-  <li><strong>Module Name</strong> — enter the name of the module;</li>
-  <li><strong>Your Site ID</strong> — enter the website ID that can be found on the
-  <a href='//www.olark.com/install' rel="nofollow" target="_blank">https://www.olark.com/install</a> page after you register and fill the required fields;</li>
-  <li><strong>Status</strong> — define the module status (enabled/disabled). </li>
+  <li><strong>Module Name</strong> — TM Footer Links Information;</li>
+  <li><strong>Links Group</strong> — Information;</li>
+  <li> <strong>Status</strong> — enabled. </li>
 </ul>
-
-<h4 align="justify">How to assign the new Olark Live Chat module to the Home Page (via Layout Builder)</h4>
-<p align="justify">Navigate to <strong>Extensions</strong> &gt; <strong>Extensions</strong>.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/edit-parallax-1.jpg" alt=""/></figure>
-</p>
-<p>Choose <strong>Modules</strong> in the <strong>Extension type</strong> dropdown.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/edit-parallax-2.jpg" alt=""/></figure>
-</p>
-<p>Find the <strong>Template Monster Layout Builder</strong> section. Navigate to the Home Page module and press the edit button.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/edit-parallax-7.jpg" alt=""/></figure>
-</p>
-<p align="justify" class="alert alert-info">Use <strong>Ctrl+F</strong> to facilitate the search process.</p>
-<p>You will be brought to the settings window:</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/edit-parallax-8.jpg" alt=""/></figure>
-</p>
-<p>Choose the position where you want to place the <strong>New Olark Live Chat </strong> module. You can add a row or a column.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/edit-parallax-9.jpg" alt=""/></figure>
-</p>
-<p>Press the <strong>Add Widget</strong> button.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/edit-parallax-11.jpg" alt=""/></figure>
-</p>
-<p> In the pop-up window choose the <strong>Olark Live Chat</strong> module to add. </p>
-<p align="justify" class="alert alert-info">Use <strong>Ctrl+F</strong> to simplify the search process.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/olark-view-6.jpg" alt=""/></figure>
-</p>
-<p>We have added the <strong>New Olark Live Chat </strong>module.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/olark-view-7.jpg" alt=""/></figure>
-</p>
-<p>Scroll the page up and click the <strong>Save</strong> button at the top right of the screen to apply the changes:
-  <figure class="img-polaroid"> <img src="img/edit-parallax-14.jpg" alt=""/></figure>
-</p>
-<p align="justify" class="alert alert-info">You can use such method to assign any module type to the home page.</p>
-<h4>How to assign the new Olark Live Chat module to any layout (via Layouts)</h4>
-<p>Navigate to <strong>Design</strong> &gt; <strong>Layouts</strong>.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/edit-parallax-15.jpg" alt=""/></figure>
-</p>
-<p align="justify">Choose the page (layout) you want the New Olark Live Chat module place on and press the edit button in the corresponding field in the action column.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/edit-parallax-17.jpg" alt=""/></figure>
-</p>
-<p>Move to the <strong>Choose the position of the modules</strong> section.</p>
-<p>Select the module position you want to place the module.</p>
-<p>Find the required module  in the dropdown.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/olark-view-8.jpg" alt=""/></figure>
-</p>
-<p>Press the &quot;Add Module&quot; button.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/olark-view-9.jpg" alt=""/></figure>
-</p>
-<p>The module was added successfully.</p>
-<p>
-  <figure class="img-polaroid"> <img src="img/olark-view-10.jpg" alt=""/></figure>
-</p>
-<p>Now you can remove and edit the module from here. Simply press the appropriate button.</p>
-<p>Scroll the page up and click the <strong>Save</strong> button at the top right of the screen to apply the changes:
-  <figure class="img-polaroid"> <img src="img/edit-parallax-21.jpg" alt=""/></figure>
-</p>
-<p align="justify" class="alert alert-info">You can use this method to assign any module type to any layout of your online store.</p>
