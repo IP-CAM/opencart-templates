64ce10d12a1d1a046f8a02a5ecea1eb2cd1ddf3c
LinguistMB <MariaBerkova.MB@gmail.com>
2017-10-26 09:46:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -73,8 +73,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             <div class="select select-version">
                 <select>
 
-                    <option data-href="#" selected>Version v1-4</option>
-                    <option data-href="/help/quick-start-guide/opencart-templates/v1-3/index_en.html">Version v1-3</option>
+                    <option data-href="/help/quick-start-guide/opencart-templates/v1-4/index_en.html" selected>Version v1-4</option
                     <option data-href="/help/quick-start-guide/opencart-templates/v1-2/index_en.html">Version v1-2</option>
                     <option data-href="/help/quick-start-guide/opencart-templates/v1-1/index_en.html">Version v1-1</option>
                     <option data-href="/help/quick-start-guide/opencart-templates/v1-0/index_en.html">Version v1-0</option>
